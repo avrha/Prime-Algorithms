9cf9474112fbdcaa77805fe9ffa240953e39d8ec
Joey Ferenchak <sjferenchak@gmail.com>
2020-05-07 20:22:38 -0400

    add trial division

---
+++ trialDivision.py
@@ -0,0 +1,12 @@
+def main():
+
+  #trial Division algorithm
+  def trialDivision(num):
+    for i in range (2,num,2):
+      print(i)
+
+  x = 10
+  trialDivision(x)
+
+if __name__ == '__main__':
+  main()
