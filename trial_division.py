68a34c0676c87de99bf3826138393522252a0d3c
Joey Ferenchak <sjferenchak@gmail.com>
2020-05-07 20:53:11 -0400

    made function return true or false

---
+++ trial_division.py
@@ -0,0 +1,17 @@
+def main():
+  #trial Division algorithm
+  def trial_division(num):
+    for i in range (2,num):
+      if (num % i) == 0:
+        return False
+      return True
+
+  #demo
+  x = 11
+  if trial_division(x) == False:
+    print(x,"is not a prime number")
+  else:
+    print(x,"is a prime number")
+
+if __name__ == '__main__':
+  main()
