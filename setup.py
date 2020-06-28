a2c34e727277863aae236ea2a1d0b3819522af8a
Joey <sjferenchak@gmail.com>
2020-06-28 00:14:20 -0400

    Add a description

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+      long_description = fh.read()
 setup(
-  name='primeTester',
-  version='0.1',
-  description='Say hello!',
+    name='primeTester',
+  version='0.2',
+  description='A collection of Python modules consisting different primality tests.',
   py_modules=["aks", "fermat",  "millerRabin", "solovayStrassen", "trialDivision"],
   package_dir={'': 'src'},
   url='https://github.com/avrha/Prime-Tests',
   liscene="MIT",
   author="Joey Ferenchak",
   author_email="sjferenchak@gmail.com",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
